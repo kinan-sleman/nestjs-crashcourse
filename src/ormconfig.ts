e1da0ce3c90c996a449955e304c9133723458bc2
Kinan Sleman <kenan@oonatech.com>
2025-11-16 07:56:03 +0300

    create tag entity

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -6,7 +6,12 @@ const config : PostgresConnectionOptions = {
     port: 5432,
     username: "devuser",
     password: "1234",
-    database: "blog"
+    database: "blog",
+    // we need to add this to support all entities that in our project
+    entities: [__dirname + '/**/*.entity.{.ts, .js}'],
+    // in each time we'll run our application, we need to refresh our database depending on our application entities
+    // in this case we need to add this line:
+    synchronize: true, // don't use it in production
 }
 
 export default config
