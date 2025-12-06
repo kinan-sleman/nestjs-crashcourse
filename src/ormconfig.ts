17979008dfb257ccd065bdf737644eae49c2d568
Kinan Sleman <kenan@oonatech.com>
2025-12-06 16:04:04 +0300

    fix an issue in PGUSER

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,11 +1,18 @@
 import { DataSource } from "typeorm";
 import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions.js";
 
+console.log({
+    PGHOST: process.env.PGHOST,
+    PGPORT: process.env.PGPORT,
+    PGUSER: process.env.PGUSER || (process.env.NODE_ENV !== 'production' ? "postgres" : undefined),
+    PGPASSWORD: process.env.PGPASSWORD,
+    PGDATABASE: process.env.PGDATABASE,
+})
 const config: PostgresConnectionOptions = {
     type: 'postgres',
     host: process.env.PGHOST || 'localhost',
     port: parseInt(String(process.env.PGPORT), 10) || 5432,
-    username: process.env.PGUSER || "postgres",
+    username: process.env.PGUSER || (process.env.NODE_ENV !== 'production' ? "postgres" : undefined),
     password: process.env.PGPASSWORD || "12345678",
     database: process.env.PGDATABASE || "blog",
     entities: [__dirname + '/**/*.entity{.ts,.js}'],
